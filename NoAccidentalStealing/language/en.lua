ff0b249b7e89b796b955d2859ded9f7d4efc2bd9
Shinni <shinni@binaryvector.net>
2018-10-22 20:17:31 +0200

    compatible with Murkmire update

--- NoAccidentalStealing/language/en.lua
+++ NoAccidentalStealing/language/en.lua
@@ -12,7 +12,7 @@ L.NAS_STEAL_INFO					= "StealInfo"
 -- addon description
 L.NAS_ADDON_NAME					= "NoAccidentalStealing"
 L.NAS_ADDON_AUTHOR					= "Shinni"
-L.NAS_ADDON_VERSION					= "1.5.15"
+L.NAS_ADDON_VERSION					= "1.6"
 
 -- option strings
 L.NAS_STEALTH_ALLOW 				= "stealth to allow stealing"
